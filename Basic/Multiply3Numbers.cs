87a155c9da52f89d1bf9543789fbad93a6dfc93b
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2026-02-18 07:49:28 +0530

    Create Multiply3Numbers.cs

---
+++ Basic/Multiply3Numbers.cs
@@ -0,0 +1,9 @@
+using System;
+class HelloWorld {
+  static void Main(){
+    int a=Convert.ToInt32(Console.ReadLine());
+    int b=Convert.ToInt32(Console.ReadLine());
+    int c=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(a * b * c);
+  }
+}
