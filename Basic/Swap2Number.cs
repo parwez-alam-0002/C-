42409f80a1428549f848e79bc165fe3aa8797efc
Shaikh Parwez Alam <parwez.alam.0618@gmail.com>
2026-02-18 07:47:05 +0530

    Create Swap2Number.cs

---
+++ Basic/Swap2Number.cs
@@ -0,0 +1,12 @@
+using System;
+class HelloWorld {
+  static void Main(){
+    //Swap Two number
+    int a=10;
+    int b=20;
+    a=a*b;
+    b=a/b;
+    a=a/b;
+    Console.WriteLine("{0}, {1}",a,b);
+  }
+}
